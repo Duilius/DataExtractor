78fac900fbc022cd23c8f5fb4cd64630595d161c
Duilio Restuccia <restucciaduilio@gmail.com>
2024-04-30 17:27:59 -0500

    Actualización 1

--- claves.py
+++ claves.py
@@ -6,8 +6,8 @@ os.environ['REGION']="us"
 
 #variables de entorno Base de Datos en Railway
 os.environ['DB_USER']="root"
-os.environ['DB_PASSWORD']="h3c6a6hGAGCA2geBGb-fh4H5FBeC2dbf"
+os.environ['DB_PASSWORD']="ScuFUeYqkYDJUyHZfalxxAKVeYmhkQmz"
 os.environ['DB_HOST']="roundhouse.proxy.rlwy.net"
-os.environ['DB_PORT']="27692"
+os.environ['DB_PORT']="37890"
 os.environ['DB_NAME']="railway"
 os.environ['DB_TYPE']="mysql+pymysql"
