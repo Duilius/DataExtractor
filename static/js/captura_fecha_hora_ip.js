62346a803d49c32ff5661277335227525f045543
Duilio Restuccia <restucciaduilio@gmail.com>
2024-04-30 17:27:59 -0500

    Actualización 1

---
+++ static/js/captura_fecha_hora_ip.js
@@ -0,0 +1,12 @@
+// Función para asignar fecha y hora a los inputs ocultos
+function asignarFechaHora() {
+    // Obtener la fecha y la hora actuales
+    var hoy = new Date();
+    var fecha = hoy.toISOString().split('T')[0]; // Formato de fecha (YYYY-MM-DD)
+    var hora = hoy.toTimeString().split(' ')[0]; // Formato de hora (HH:MM:SS)
+
+    // Asignar valores a los campos ocultos del formulario
+    document.getElementById('laFecha').value = fecha;
+    document.getElementById('laHora').value = hora;
+}
+
