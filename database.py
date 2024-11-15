f171fe5f9d4ac95edadfcad8ca87306a11d807ad
Duilio Restuccia <restucciaduilio@gmail.com>
2024-11-15 01:59:55 -0500

    Agregado módulo database

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+import claves  # Importa tus variables de entorno
+
+# Usa tus variables de entorno para la conexión
+# Variables de conexión a Base de Datos en Railway
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_type = os.getenv("DB_TYPE")
+
+engine = create_engine(f'{db_type}://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
